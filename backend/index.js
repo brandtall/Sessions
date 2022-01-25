aed3790694b2e511f317cad5d2a1ec10f0cc7c9d
Brandtall <webyahyaver@gmail.com>
2022-01-25 19:16:06 +0400

    Replace student router with user router. Add socketio implementation

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
 const express = require('express');
-const studentRouter = require('./controllers/studentController')
+const http = require("http");
 const registerRouter = require('./controllers/registerRouter');
 const loginRouter = require('./controllers/loginController');
 const cors = require('cors');
+const socketIo = require("socket.io");
+const userRouter = require('./controllers/userController');
 const app = express();
+const server = http.createServer(app);
 
 const connectDB = async() => {
   try {
@@ -19,7 +22,7 @@ const connectDB = async() => {
 connectDB();
 app.use(cors());
 app.use(express.json());
-app.use('/students', studentRouter);
+app.use('/users', userRouter);
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
 app.get('/', (request, response) => {
@@ -30,3 +33,16 @@ app.get('/', (request, response) => {
 app.listen(process.env.PORT, () => {
   console.log("Listening at port " + process.env.PORT);
 });
+
+server.listen(3005);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+io.on("connection", (socket) => {
+  console.log("new user");
+  io.emit("FromAPI", "Hi frontend");
+  
+})
