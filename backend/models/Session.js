d2320078d7679f829e589ca256b8b92ca91b54a2
Brandtall <webyahyaver@gmail.com>
2022-01-25 19:14:43 +0400

    Add session ID and unique validator

--- backend/models/Session.js
+++ backend/models/Session.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const sessionSchema = new mongoose.Schema({
+  sessionId: {
+    type: String,
+    required: true,
+    unique: true
+  },
   title: {
     type: String,
     required: true
@@ -15,6 +21,16 @@ const sessionSchema = new mongoose.Schema({
     ref: 'Instructor'
   }
 });
+
+sessionSchema.plugin(uniqueValidator);
+
+sessionSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+})
 
 const Session = mongoose.model('Session', sessionSchema);
 
