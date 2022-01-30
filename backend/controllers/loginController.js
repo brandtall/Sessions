f122660d1e78d02ba72844d0fc99d32516bf7e34
Brandtall <webyahyaver@gmail.com>
2022-01-30 08:55:51 +0400

    Fix multiple responses bug by addding if-else chain for error detection.

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -7,18 +7,20 @@ const loginRouter = express.Router();
 loginRouter.post('/', async (request, response) => {
   const userId = request.body.userId;
   const password = request.body.password;
-  const user = await User.findOne({userId}).populate('courses');
-  console.log(user)
+  const user = await User.findOne({ userId }).populate('courses');
   if (!user) {
-    response.json({error: "User ID is incorrect"});
+    response.json({ error: "User ID is incorrect" });
+  } else {
+    const passwordHash = user.passwordHash;
+    const compare = await bcrypt.compare(password, passwordHash);
+    if (!compare) {
+      response.json({ error: "Password is incorrect" });
+    } else {
+      const token = jwt.sign({ user }, process.env.PRIVATE_KEY, { expiresIn: '1h' });
+      response.status(200).json({ token, courses: user.courses })
+    }
   }
-  const passwordHash = user.passwordHash;
-  const compare = await bcrypt.compare(password, passwordHash);
-  if(!compare) {
-    response.json({error: "Password is incorrect"});
-  }
-  const token = jwt.sign({user}, process.env.PRIVATE_KEY, {expiresIn: '1h'});
-  response.status(200).json({token, courses: user.courses})
+
 })
 
 module.exports = loginRouter;
