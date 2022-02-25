6333d1d62a4f86ef011edcff9394d5536df6ce8a
Brandtall <webyahyaver@gmail.com>
2022-02-25 17:50:46 +0400

    Add video display capablities for user video

---
+++ frontend/src/components/Courses/IndividualCourse/Sessions/Room/userVideo.js
@@ -0,0 +1,20 @@
+import { createRef, useEffect } from "react"
+
+
+const UserVideo = (props) => {
+    const localVideo = createRef();
+    useEffect(() => {
+        localVideo.current.srcObject = props.localStream;
+        localVideo.current.onloadedmetadata = (e) => {
+            localVideo.current.play();
+          }
+    }, [])
+    return (
+        <div>
+            <video ref={localVideo} autoPlay playsInline controls={false}/>
+            <p>Inside UserVideo</p>
+        </div>
+    );
+}
+
+export default UserVideo;
