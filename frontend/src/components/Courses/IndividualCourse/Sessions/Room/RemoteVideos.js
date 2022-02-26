0beca8a2db6db88adbc91a161732705e4f5d0773
Brandtall <webyahyaver@gmail.com>
2022-02-26 15:48:48 +0400

    Add functions to set remote streams and display

--- frontend/src/components/Courses/IndividualCourse/Sessions/Room/RemoteVideos.js
+++ frontend/src/components/Courses/IndividualCourse/Sessions/Room/RemoteVideos.js
@@ -1,2 +1,42 @@
+import { createRef, useEffect, useState } from "react";
+
 const RemoteVideos = (props) => {
+    const [remoteVideosList, setremoteVideosList] = useState([]);
+    
+    useEffect(() => {
+        updateStreams();
+    }, [])
+
+    useEffect(() => {
+        showPeers();
+    }, [props.remoteStream])
+
+
+    const updateStreams = () => {
+        const updatedRemoteStreams = [];
+        Object.keys(props.peer.connections).forEach((e) => {
+            updatedRemoteStreams.push(props.peer.connections[e][0]._remoteStream);
+            setremoteVideosList([...RemoteVideos, createRef()])
+        });
+        props.setRemoteStream([...updatedRemoteStreams]);
+    }
+    const showPeers = () => {
+        props.remoteStream.forEach((e) => {
+            const newRemoteVideo = createRef();
+            newRemoteVideo.current.srcObject = e;
+        })
+    }
+
+
+    return (
+        <div>
+            {remoteVideosList.map((rv) => {
+                return (
+                    <video ref={rv} autoPlay playsInline controls={false} />
+                );
+            })}
+        </div>
+    );
 }
+
+export default RemoteVideos;
