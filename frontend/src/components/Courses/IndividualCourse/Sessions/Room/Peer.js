6c006e5038467483527230141ecc7c37d8c1f607
Brandtall <webyahyaver@gmail.com>
2022-02-28 09:02:46 +0400

    Fix duplicate remote video streams. Remove video after user disconnects. Change peer id to socket id.

--- frontend/src/components/Courses/IndividualCourse/Sessions/Room/Peer.js
+++ frontend/src/components/Courses/IndividualCourse/Sessions/Room/Peer.js
@@ -3,40 +3,33 @@ import { useEffect, useState } from 'react';
 import Socket from 'socket.io-client';
 import { v4 as uuidv4 } from 'uuid';
 import Call from './Call';
+import Message from './Chat/Message';
+import MessageInput from './Chat/MessageInput';
 import RemoteVideos from './RemoteVideos';
 import UserVideo from './userVideo';
-const localId = uuidv4();
+let localId;
 
-
-const PeerComponent = () => {
+const PeerComponent = (props) => {
     const [remoteStream, setRemoteStream] = useState([]);
     const [myStream, setMyStream] = useState(null);
     const [socket, setSocket] = useState(null);
     const [myId, setMyId] = useState("");
     const [socketId, setSocketId] = useState("");
     const [peer, setPeer] = useState(null);
+    const [response, setResponse] = useState([])
+    const [message, setMessage] = useState("");
     useEffect(() => {
         let io = new Socket('http://localhost:3003', { withCredentials: true });
-        setSocket(io);
-        setMyId(localId);
-        const localPeer = new Peer(localId);
-        setPeer(localPeer);
-        getVideo();
         io.on("connect", () => {
             setSocketId(io.id);
+            localId = io.id;
+            const localPeer = new Peer(localId);
+            setPeer(localPeer);
         });
+        setSocket(io);
+        getVideo();
         return () => io.disconnect();
     }, []);
-
-    // useEffect(() => {
-    //     if(peer) {
-    //         const updatedRemoteStreams = [];
-    //         Object.keys(peer.connections).forEach((e) => {
-    //             updatedRemoteStreams.push(peer.connections[e][0]._remoteStream);
-    //         });
-    //         setRemoteStream([...updatedRemoteStreams]);
-    //     }
-    // }, [peer])
 
     const getVideo = async () => {
         const localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
@@ -45,36 +38,64 @@ const PeerComponent = () => {
 
     const makeCall = async (arg) => {
         const localCall = await peer.call(arg.peerId, myStream);
-        console.log(localCall);
         localCall.on("stream", (stream) => {
-            if(stream.id !== myStream.id) {
-                setRemoteStream((prevArray) => [...prevArray, stream]);
+            setRemoteStream((prevArray) => {
+                const newArray = prevArray.filter((e) => e.id !== stream.id);
+                return [...newArray, stream];
+
             }
+            );
         });
+        socket.on("closeCall", (arg) => {
+            setRemoteStream((prevArray) => {
+                const newArray = prevArray.filter((e, i) => e.id !== peer.connections[arg][0]._remoteStream.id);
+                return [...newArray];
+            });
+        });
+        peer.on("close", () => {
+            peer.destroy();
+        })
         socket.emit("makeCall", { "to": arg.from });
     }
     const answerCall = async () => {
         peer.on("call", (call) => {
             const answer = call.answer(myStream);
-            console.log("Answer")
             call.on("stream", (stream) => {
-                if(stream.id !== myStream.id) {
-                    setRemoteStream((prevArray) => [...prevArray, stream]);
+                setRemoteStream((prevArray) => {
+                    const newArray = prevArray.filter((e) => e.id !== stream.id);
+                    return [...newArray, stream];
                 }
+                );
             });
-            console.log(peer.connections);
+            socket.on("closeCall", (arg) => {
+                setRemoteStream((prevArray) => {
+                    const newArray = prevArray.filter((e, i) => e.id === peer.connections[arg][0]._remoteStream.id);
+                    return [...newArray];
+                });
+            });
+            peer.on("close", () => {
+                peer.destroy();
+            })
         });
     }
     return (
         <div>
-            {socket && myStream && peer && myId ?
+            {socket && myStream && peer && socketId ?
                 <div>
-                    <Call socket={socket} myId={myId} peer={peer} socketId={socketId} makeCall={makeCall} answerCall={answerCall} />
+                    <Call socket={socket} peer={peer} socketId={socketId} makeCall={makeCall} answerCall={answerCall} />
                     <UserVideo myStream={myStream} />
-                    {remoteStream ? <RemoteVideos remoteStream={remoteStream} peer={peer} remoteStream={remoteStream} setRemoteStream={setRemoteStream}/>
-                    : <div></div>} 
+                    {remoteStream ? <RemoteVideos remoteStream={remoteStream} peer={peer} remoteStream={remoteStream} setRemoteStream={setRemoteStream} />
+                        : <div></div>}
                 </div>
                 : <div></div>}
+            {socket ?
+                <div>
+                    <Message socket={socket} message={message} setMessage={setMessage} response={response} setResponse={setResponse}
+                    />
+                    <MessageInput socket={socket} message={message} setMessage={setMessage} response={response} setResponse={setResponse}
+                        userName={props.userName} />
+                </div>
+                : <p>No Socket Connection Yet...</p>}
         </div>
     )
 }
